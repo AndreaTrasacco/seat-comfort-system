e2396d2a94fbaa07290f3ddd7d0b3d4c3c5af488
Andrea1900 <66999436+Andrea1900@users.noreply.github.com>
2024-01-26 21:53:34 +0100

    Tested and corrected image picker

--- seatcomfortlogic/seat_comfort_controller.py
+++ seatcomfortlogic/seat_comfort_controller.py
@@ -3,14 +3,11 @@ import time
 import tkinter as tk
 
 import numpy as np
-import requests
+import json
+import socket
 
-from eyesdetection.eyes_detector import EyesDetector
 from gui.camera_view import CameraView
-from gui.rigth_side_view import RightSideView
-from gui.textfield_view import TextFieldView
 from seatcomfortlogic.users_storage_controller import UsersStorageController
-from userrecognition.user_recognizer import UserRecognizer
 
 # lock to ensure mutual exclusion for the access of the frame
 shared_frame = threading.Lock()
@@ -21,40 +18,68 @@ class SeatComfortController:
     def __init__(self):
         # initialize the GUI
         self.master = tk.Tk()
-        self.textfield_view = TextFieldView(self.master, self)
+        # self.textfield_view = TextFieldView(self.master)
         self.camera_view = CameraView(self.master)
-        self.right_side_view = RightSideView(self.master)
+        # self.right_side_view = RightSideView(self.master)
 
-        self.camera_endpoint = "http://169.254.101.5:5000/Raspberry/photo"
-        self.RECEIVE_PHOTO_INTERVAL = 1
+        self.camera_endpoint = "http://169.254.101.5:5000/Raspberry/photo"  # TODO mettere corretto
 
         self._users_storage_controller = UsersStorageController()
         self._users = self._users_storage_controller.retrieve_users()
-        self._need_detector = EyesDetector()
-        self._user_recognizer = UserRecognizer()
+        # self._need_detector = EyesDetector()
+        # self._user_recognizer = UserRecognizer()
 
     def get_capture(self):
-        while True:
-            time.sleep(self.RECEIVE_PHOTO_INTERVAL)
-            response = requests.get(self.camera_endpoint)
-            json_data = response.json()
-            with shared_frame:
-                actual_frame = np.array(json_data['photo'])
-                self.camera_view.update_image(actual_frame)
+        # Server configuration
+        host = '169.254.101.5'
+        port = 8000
+
+        # Create a socket object
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect to the server
+        client_socket.connect((host, port))
+
+        try:
+            while True:
+                time.sleep(0.05)  # TODO METTERE IN UNA COSTANTE
+                # Send a message to the server
+                message_to_send = {"message": "request_for_photo"}
+                client_socket.send(json.dumps(message_to_send).encode())
+                # Receive the welcome message from the server
+
+                # Receive the actual image data
+                # image_data = client_socket.recv(1555200)
+                image_data = b''
+                while len(image_data) < 1555200:
+                    data = client_socket.recv(1555200 - len(image_data))
+                    if not data:
+                        break  # Connection closed
+                    image_data += data
+
+                # Convert the received data to a numpy array
+                image_np = np.frombuffer(image_data, dtype=np.uint8)
+                # Reshape the NumPy array to the original image shape
+                image = image_np.reshape((960, 540, 3))
+                with shared_frame:
+                    self.camera_view.update_image(image)
+
+        except KeyboardInterrupt:
+            print("Client interrupted by keyboard. Closing connection.")
+            client_socket.close()
 
     def main(self):
+        # Create the view
         # Start thread for capturing frames
         camera_thread = threading.Thread(target=self.get_capture)
         camera_thread.daemon = True
         camera_thread.start()
 
-        # create view
-        self.master.mainloop()
+        self.master.mainloop() # TODO FAR PARTIRE CON THREAD
 
     def signup_button_handler(self):
         name = self.textfield_view.get_text()
-        print(name)
-        # TODO GET img FROM CAMERA - ATTENTION
+        # TODO GET img USING LOCK
         img = []
         if name != '':
             self._user_recognizer.register_user(name, img)
