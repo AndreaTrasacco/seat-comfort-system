6f52e9709ace51819f9cacb2c387b3df55184548
Andrea1900 <66999436+Andrea1900@users.noreply.github.com>
2024-01-27 14:20:06 +0100

    Tested and corrected image picker

--- imagepicker/image_picker.py
+++ imagepicker/image_picker.py
@@ -1,14 +1,12 @@
 import io
 import time
+import sys
+import socket
+import json
 
 import numpy as np
 from PIL import Image
-from flask import Flask
-from flask_restful import Api
 from picamera import PiCamera
-
-app = Flask(__name__)
-api = Api(app)
 
 
 class ImagePicker:  # Flask server listening for GET requests for photos (captured at the moment of request)
@@ -16,6 +14,7 @@ class ImagePicker:  # Flask server listening for GET requests for photos (captur
         self._camera = PiCamera()
         time.sleep(2)
         self._camera.resolution = (540, 960)
+        self._camera.capture("img.jpg", format="jpeg")
 
     def capture_image(self):  # Capture the image and returns it as a numpy array
         img = io.BytesIO()
@@ -23,10 +22,44 @@ class ImagePicker:  # Flask server listening for GET requests for photos (captur
         img_pil = Image.open(img)
         return np.array(img_pil)
 
-    def get(self):
-        return {'photo': self.capture_image()}
+    def main(self):
+        # Server configuration
+        host = '169.254.101.5'
+        port = 8000
+
+        # Create a socket object
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Bind the socket to a specific address and port
+        server_socket.bind((host, port))
+
+        # Listen for incoming connections (max 1 connection in the queue)
+        server_socket.listen(1)
+
+        print(f"Server listening on {host}:{port}")
+
+        # Accept a connection from a client
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address}")
+
+        try:
+            while True:
+                # Receive data from the client
+                data = client_socket.recv(1024).decode()
+                # received_data = json.loads(data)
+
+                # Serialize the NumPy array
+                serialized_data = self.capture_image().tobytes()
+
+                # Send the actual serialized data
+                client_socket.sendall(serialized_data)
 
 
-api.add_resource(ImagePicker, '/Raspberry/photo')
+        except KeyboardInterrupt:
+            print("Server interrupted by keyboard. Closing connection.")
+            client_socket.close()
+            server_socket.close()
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='169.254.101.5')
+    ImagePicker().main()
